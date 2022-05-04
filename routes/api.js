ab758b1427214c9d0398153ddef84692c8877944
lautarog1596 <lautarog1596@gmail.com>
2022-05-03 23:56:40 -0300

    DELETE /api/issues/:project

--- routes/api.js
+++ routes/api.js
@@ -143,7 +143,32 @@ module.exports = app => {
 
     .delete((req, res) => {
         let project = req.params.project;
+        const { _id } = req.body;
+        if (!_id) {
+            res.json({ error: "missing _id" });
+            return;
+        }
+        ProjectModel.findOne({ name: project }, (err, projectdata) => {
+            if (err || !projectdata) {
+                res.json({ error: "could not delete", _id: _id })
+                return;
+            } else {
+                const issueData = projectdata.issues.id(_id)
+                if (!issueData) {
+                    res.json({ error: "could not delete", _id: _id })
+                    return;
+                }
+                issueData.remove();
 
+                projectdata.save((err, data) => {
+                    if (err || !data) {
+                        res.json({ error: "could not delete", _id: _id })
+                    } else {
+                        res.json({ result: "successfully deleted", _id: _id })
+                    }
+                });
+            }
+        });
     });
 
 };
